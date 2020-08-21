1f6d18677e509a4d499d8d6fb4a1da426938c477
Jose Rogerio de Almeida Junior <almeidajr.vr@gmail.com>
2020-08-21 12:53:54 -0300

    Creating a console calculator in C++

--- CalculatorTutorial/CalculatorTutorial.cpp
+++ CalculatorTutorial/CalculatorTutorial.cpp
@@ -3,9 +3,38 @@
 
 #include <iostream>
 
+#include "Calculator.h";
+
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
+    char oper = '+';
+
+    cout << "Calculator Console Application" << endl << endl;
+    cout << "Please enter the operation to perform." << endl;
+    cout << "Format: a+b | a-b | a*b | a/b" << endl;
+
+    Calculator c;
+    while (true)
+    {
+        cin >> x >> oper >> y;
+        if (oper == '/' && y == 0)
+        {
+            cout << "Division by 0 exception" << endl;
+            continue;
+        }
+        else
+        {
+            result = c.Calculate(x, oper, y);
+        }
+        cout << "Result is: " << result << endl;
+    }
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
